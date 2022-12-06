06fe4684fea0616d1fa6ba5fb73bdb3176c06f6f
phebaleb <phoebe.thompson02@gmail.com>
2022-12-07 11:26:44 +1300

    testing links 2

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -15,19 +15,19 @@ const Projects = () => {
             </div>
 
             <div className="project-card">
-                <a href="https://phoebes-portfolio2022.vercel.app/phoebejomusic.vercel.app"><div className="project-image music"></div></a>
+                <a href="phoebejomusic.vercel.app"><div className="project-image music"></div></a>
                 <h2>PHOEBE JO MUSIC</h2>
                 <p>2022</p>
             </div>
 
             <div className="project-card">
-                <a href="https://phoebes-portfolio2022.vercel.app/eworld-portfolio.vercel.app"><div className="project-image ehome"></div></a>
+                <a href="eworld-portfolio.vercel.app"><div className="project-image ehome"></div></a>
                 <h2>EHOME BY EWORLD</h2>
                 <p>2022</p>
             </div>
 
             <div className="project-card">
-                <a href="https://phoebes-portfolio2022.vercel.app/montys.vercel.app"><div className="project-image montys"></div></a>
+                <a href="montys.vercel.app"><div className="project-image montys"></div></a>
                 <h2>MONTYS</h2>
                 <p>2022</p>
             </div>
